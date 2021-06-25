5d4d498ff9a9104b9480c37f636d5c1a7dc0b493
ashwin25march <ashwin.25march@gmail.com>
2021-06-25 20:31:03 +0530

    while loop

--- src/java/4 while/Hello4.java
+++ src/java/4 while/Hello4.java
@@ -1,6 +1,12 @@
 public class Hello4 {
     public static void main(String[] args) {
-    
+        int count=0;
+        while(count <100){
+            count++;
+            System.out.println("aasshhwwiinn");
+            System.out.println(count);
+        }
+
     }
 }
 
